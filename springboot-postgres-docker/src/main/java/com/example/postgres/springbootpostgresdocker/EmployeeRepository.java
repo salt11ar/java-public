bd2089fe5456bf34f42855362b2c7fc0ef8bcee3
salt11ar <salt11ar@gmail.com>
2022-06-27 20:44:50 -0300

    Added  transfer service to test Isolation in DB

--- springboot-postgres-docker/src/main/java/com/example/postgres/springbootpostgresdocker/EmployeeRepository.java
+++ springboot-postgres-docker/src/main/java/com/example/postgres/springbootpostgresdocker/EmployeeRepository.java
@@ -1,20 +1,39 @@
 package com.example.postgres.springbootpostgresdocker;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
+import com.example.postgres.springbootpostgresdocker.Model.Employee;
 import java.util.Collection;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.example.postgres.springbootpostgresdocker.Model.Employee;
-
+@Repository 
+@Transactional(readOnly = true)
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	
-    @Query(value = "SELECT * from employee WHERE email = :mail", nativeQuery = true) 
-    public List<Employee> findAllByEmail(@Param("mail") String email);
+  @Query(
+    value = "SELECT * from employee WHERE email = :mail",
+    nativeQuery = true
+  )
+  public List<Employee> findAllByEmail(@Param("mail") String email);
 
-   /* @Query("SELECT u FROM employee u WHERE u.salary > 0")
+  @Query(
+    value = "SELECT salary from employee WHERE email = :mail",
+    nativeQuery = true
+  )
+  long getSalary(@Param("mail") String email);
+
+  @Query(
+    value = " UPDATE employee SET salary = salary + :cents WHERE email = :mail",
+    nativeQuery = true
+  )
+  @Modifying
+  @Transactional
+  int addBalance(@Param("mail") String mail, @Param("cents") long cents);
+  
+  /* @Query("SELECT u FROM employee u WHERE u.salary > 0")
     public Collection<Employee> findAllActiveEmployees();
     
     @Query(value = "SELECT * FROM EMPLOYEE u WHERE u.salary > 0", nativeQuery = true)
--- springboot-postgres-docker/src/test/java/com/example/postgres/springbootpostgresdocker/SpringbootPostgresDockerApplicationTests.java
+++ springboot-postgres-docker/src/test/java/com/example/postgres/springbootpostgresdocker/SpringbootPostgresDockerApplicationTests.java
@@ -1,13 +1,77 @@
 package com.example.postgres.springbootpostgresdocker;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.CountDownLatch;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.postgres.springbootpostgresdocker.Model.Employee;
+import com.example.postgres.springbootpostgresdocker.services.TransferServiceImpl;
 
 @SpringBootTest
 class SpringbootPostgresDockerApplicationTests {
+
+	//private final Logger LOGGER = LoggerFactory.getLogger(SpringbootPostgresDockerApplicationTests.class);
+
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private TransferServiceImpl transferServiceImpl;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testParallelExecution()
+        throws InterruptedException {
+    
+			contextLoads();
+
+			Employee alice = new Employee();
+			alice.setEmail("Alice-123");
+			alice.setSalary(10D);
+			employeeRepository.save(alice);
+
+			Employee bob = new Employee();
+			bob.setEmail("Bob-456");
+			bob.setSalary(0D);
+			employeeRepository.save(bob);
+			
+			double d = employeeRepository.getSalary("Alice-123");
+
+			assertEquals(10D, d);
+			assertEquals(0D, employeeRepository.getSalary("Bob-456"));
+		
+			CountDownLatch startLatch = new CountDownLatch(1);
+			int threadCount= 8;
+			CountDownLatch endLatch = new CountDownLatch(threadCount);
+		
+			for (int i = 0; i < threadCount; i++) {
+				new Thread(() -> {
+					try {
+						startLatch.await();
+		
+						transferServiceImpl.transfer(
+							"Alice-123", "Bob-456", 5L
+						);
+					} catch (Exception e) {
+						//logger.error("Transfer failed", e);
+						e.printStackTrace();
+					} finally {
+						endLatch.countDown();
+					}
+				}).start();
+			}
+			startLatch.countDown();
+			endLatch.await();
+			
+//			LOGGER.info("Alice's balance {}"+ employeeRepository.getSalary("Alice-123"));
+			//LOGGER.info("Bob's balance {}"+ employeeRepository.getSalary("Bob-456"));
 }
+}
+
