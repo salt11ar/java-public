eb83a4414b53011bb6e65d3f84da423ed5c93d51
salt11ar <salt11ar@gmail.com>
2022-09-13 19:22:07 -0300

    new code challenge

--- challenges/challenges/src/codilitySmallestPosIntegerArray java.java
+++ challenges/challenges/src/codilitySmallestPosIntegerArray java.java
@@ -1,4 +1,7 @@
-
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This is a demo task.
@@ -49,14 +52,131 @@ class codilitySmallestPosIntegerArray {
 
     public static void main(String[] args) {
 
-        int[] i1 = new int[121];
+        /* int[] i1 = new int[121];
 
         i1[0] = -1;
         i1[1] = -1;
-        solution(i1);
+        solution(i1); */
+
+        System.out.println("-> "+ test1());
+        System.out.println("-> "+ test2());
+        System.out.println("-> "+ test3());
+        System.out.println("-> "+ test4());
+        
     }
     public static int solution(int[] A) {
         // write your code in Java SE 8
+        // optimize later for space and time?
+
+        // solution design 1. order array  by sort
+        // 2. get rid of duplicates, maybe send to a set or streams
+        // 3. for each element see if it is the previous -1 if not it is the one 
+        // 4. check negatives and boundaries if no gap found in the list 
+
+        Comparator<Integer> byAsc = (Integer o1, Integer o2) -> o1.compareTo(o2);
+        
+        
+        List<Integer> alist = new ArrayList<>();
+        for (int val : A) {
+            alist.add(val);
+        }
+        
+        System.out.println("list w integers: " + alist);
+        
+        alist.sort(byAsc);
+        alist = alist.stream()
+                .distinct()
+                .collect(Collectors.toList());         
+
+        System.out.println("list distinct and ordered:" + alist);
+
+        
+        boolean foundgap=false;
+        //follow list from start 
+        for (int i = 0; i < alist.size()-1; i++){
+
+            System.out.println("i= "+alist.get(i)+ ", i+1= "+ alist.get(i+1));
+
+            if ( (alist.get(i) > 0 ) && 
+                    (alist.get(i+1) - alist.get(i) != 1 )
+                     ) // i and i+1 should be +1 in the value
+            {
+                    System.out.println("gap");
+                    foundgap = true;
+                    return alist.get(i).intValue()+1;
+            }
+        }
+
+        int lastelem = alist.get(alist.size()-1);
+        
+        if(foundgap == false)
+        {   if(lastelem < 0)
+                return 1;            
+            else
+                return lastelem + 1;
+        }
+
         return 1;
+        
+    }
+
+    public static boolean test1()
+    {
+        int[] thearray= new int[]{1, 3, 6, 4, 1, 2};
+        System.out.println("test input" );
+        printArrayToConsole(thearray); 
+        
+        
+        if (solution(thearray) == 5) 
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean test2()
+    {
+        int[] thearray= new int[]{1, 2, 3};
+        System.out.println("test input" );
+        printArrayToConsole(thearray); 
+        
+        
+        if (solution(thearray) == 4) 
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean test3()
+    {
+        int[] thearray= new int[]{Integer.valueOf(-1), Integer.valueOf(-3)};
+        System.out.println("test input" );
+        printArrayToConsole(thearray); 
+        
+        if (solution(thearray) == 1) 
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean test4()
+    {
+        int[] thearray= new int[]{1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,20};
+        System.out.println("test input" );
+        printArrayToConsole(thearray); 
+        
+        
+        if (solution(thearray) == 15) 
+            return true;
+        else
+            return false;
+    }
+    
+    public static void printArrayToConsole(int[] intArray) {
+        System.out.print("[");
+        for (int i = 0; i < intArray.length; i++)
+            System.out.print(intArray[i] + " ");
+        System.out.print("]");
+        System.out.println();
+
     }
 }
